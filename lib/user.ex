a5828a83a084186f87a7c41f12d4122b26fc257d
Jeffrey Matthias <jeffrey@community.com>
2020-04-06 16:19:54 -0600

    First test passed.

--- lib/user.ex
+++ lib/user.ex
@@ -1,3 +1,13 @@
 defmodule IdleTesting.User do
   use IdleTesting.Schema
+
+  schema "users" do
+    field(:email, :string)
+    field(:favorite_number, :integer)
+    field(:first_name, :string)
+    field(:last_name, :string)
+    timestamps()
+  end
+
+  def fields, do: __MODULE__.__schema__(:fields)
 end
